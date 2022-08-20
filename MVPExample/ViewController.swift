dafae523b97babf8f225d7ff2757eeeb1ec12841
Maksim Botalov <dev.botalov@gmail.com>
2022-08-21 00:08:35 +0500

    Initial Commit

---
+++ MVPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPExample
+//
+//  Created by Максим Боталов on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
